0b0d75fb979012142d1e630ad7e0d9ff4d35e03e
soar.L <783269447@qq.com>
2018-02-09 19:45:45 +0800

    new file

--- Main.java
+++ Main.java
@@ -8,18 +8,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        //测试文件分割
+        //测试文件分割合并
        fileSplitTest();
     }
 
     private static void fileSplitTest() throws IOException {
         //源文件
-        File source = new File(baseDir.getAbsolutePath() + File.separator + "video.mp4");
+        File source = new File(baseDir.getAbsolutePath() + File.separator + "ubuntu16.04.3.iso");
         //单张图片能够容纳的最大数据量
-        long maxLength = 4 * 1024 * 1024;
+        long maxLength = (long) (4.5 * 1024 * 1024);
         //测试将一个文件分割成多张图片
+        long time = System.currentTimeMillis();
         SplitList splitList = FileAnalysisUtil.split(source, baseDir, maxLength, "png");
+        time = System.currentTimeMillis() - time;
+        System.out.println("split :" + time);
+        time = System.currentTimeMillis();
         //将被分割的多张图片还原成一个文件，要求图片必须是原图，未经过任何处理，压缩，转码，修改等
         File target = FileAnalysisUtil.merge(splitList, baseDir, "out_");
+        time = System.currentTimeMillis() - time;
+        System.out.println("merge :" + time);
     }
 }
